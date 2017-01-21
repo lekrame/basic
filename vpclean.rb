ad92d2b69731753ca38b10deb222181acce27f4d
Michael Kramer <git@pianospree.com>
2017-01-20 23:30:00 -0800

    working

--- vpclean.rb
+++ vpclean.rb
@@ -23,8 +23,9 @@ end
 
 ##### find the VPC
 waitr = Aws::Waiters::Waiter.new({:delay => 2, :max_attempts => 60})
-@client = Aws::EC2::Client.new
-resp = @client.describe_vpcs
+@owclient = Aws::OpsWorks::Client.new
+@ec2client = Aws::EC2::Client.new
+resp = @ec2client.describe_vpcs
 resp.vpcs.each do |vpc|
 	@nameIdx = vpc.tags.find_index{|tt| tt.key == "Name"}
 	if vpc.tags[@nameIdx].value == @vpcname
@@ -44,98 +45,55 @@ def loseTheSubnet(subnetId)
 	if @options[:verbose]
 		puts "delete subnet with id #{subnetId}"
 	end
-	@client.delete_subnet({
+	@ec2client.delete_subnet({
  		subnet_id: subnetId 
 	})
 	sleep 2
+end
+
+##### collect opsworks structures to kill
+# first, find all stacks in the vpc
+mystacks = []
+@stackids_to_delete = []
+mystacks = @owclient.describe_stacks().stacks
+mystacks.each do |stk|
+	@stackids_to_delete.push(stk.stack_id) if stk.vpc_id == @vpcid
 end
 
 ##### collect instances to kill
 if @options[:verbose]
 	puts "terminate any instances"
 end
-@i_resp = @client.describe_instances
-alive = []
-@i_resp.reservations.each do |rr| # get a list of active (alive) instances
-	rr.instances.each do |ii|
-		if ii.state.name != "terminated"
-			alive.push( {"id" => ii.instance_id, "subnetid" => ii.network_interfaces[0].subnet_id, "ni" => ii.network_interfaces[0].network_interface_id})
-		end
+@instances_to_delete = []
+@stackids_to_delete.each do |sid|
+	layerids_to_delete = []
+	instanceids_to_delete = []
+	@instances_to_delete.push(@owclient.describe_instances({ stack_id: sid }).instances)
+#	@instances_to_delete.sort!.uniq!
+	@instances_to_delete.each do |inst|
+		@owclient.stop_instance({instance_id: inst.instance_id})
+		instanceids_to_delete.push(inst.instance_id)
 	end
+	@owclient.wait_until(:instance_stopped, {:instance_ids => instanceids_to_delete})
+	instanceids_to_delete.each do |iid|
+		@owclient.terminate_instance({instance_id: iid})
+	end
+	@owclient.wait_until(:instance_terminated, {:instance_ids => instanceids_to_delete})
+	layers_to_delete = @owclient.describe_layers({stack_id: sid }).layers
+	layers_to_delete.each do |layer|
+		layerids_to_delete.push(layer.layer_id)
+		@owclient.delete_layer({layer_id: layer.layer_id})
+	end
+	@owclient.delete_stack(stack_id: sid)
 end
 
-##### collect network interfaces
-resp = @client.describe_network_interfaces
-ni_list=[]
-resp[:network_interfaces].each do |ni|
-	ni_list.push {ni.network_interface_id, ni.subnet_id}
-end
-puts "network interfaces:\n"
-p ni_list
-exit
 
-##### terminate instances
-@opc = Aws::OpsWorks::Client.new(region: 'us-west-1')
-sbnts = @client.describe_subnets
+##### delete subnets
+sbnts = @ec2client.describe_subnets
 sbnts.subnets.each do |sbnt|
 	if sbnt.vpc_id == @vpcid
-		alive.each do |ii|
-			if ii["subnetid"] == sbnt.subnet_id
-				@client.terminate_instances(instance_ids: [ii["id"]])
-				puts "terminating instance id = %s" % ii["id"]
-				not_ready = true
-				while not_ready do
-					my_inst = @client.describe_instances({instance_ids: [ii["id"]]})
-#					puts "Check instance id #{ii['id']}"
-					if my_inst.reservations[0].instances[0].state.name == "terminated"
-						not_ready = false
-						puts "instance is now in state #{my_inst.reservations[0].instances[0].state.name}"
-					else
-#						puts "wait 4 more secs"
-						sleep 4
-					end
-				end
-				puts "delete net interface id #{ii['ni']}"
-				@client.delete_network_interface({
-				  network_interface_id: ii["ni"]
-				})
-			end
-		end
 		loseTheSubnet(sbnt.subnet_id)
 	end
 end
 
-=begin
-if @options[:verbose]
-	puts "delete subnets"
-end
-nis = @client.describe_network_interfaces
-begin
-	sbnts.subnets.each do |sbnt|
-		if sbnt.vpc_id == @vpcid
-			loseTheSubnet(sbnt.subnet_id)
-		end
-	end
-rescue 'InvalidSubnetIDNotFound'
-	puts "subnet id %s not found" 
-ensure
-	puts "proceeding"
-	sbnts = @client.describe_subnets
-	sbnts.subnets.each do |sbnt|
-		if sbnt.vpc_id == @vpcid
-			sid = sbnt.subnet_id
-			loseTheSubnet(sid)
-			nis.network_interfaces.each do |ni|
-				if ni.subnet_id == sid
-					@client.delete_network_interface({
-					  network_interface_id: ni.network_interface_id 
-					})
-				end
-			end
-		end
-	end
-	sleep 2
-end
-=end
-sleep 2
-@client.delete_vpc(:vpc_id => @vpcid)
+@ec2client.delete_vpc(:vpc_id => @vpcid)
