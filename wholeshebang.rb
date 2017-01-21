3886b232bd6f82187fcb0b1316b95b3e89baa3f8
Michael Kramer <git@pianospree.com>
2017-01-21 09:49:28 -0800

    misc lots of changes.  who cares, but saved anyway

--- wholeshebang.rb
+++ wholeshebang.rb
@@ -48,8 +48,6 @@ subnet0.wait_until(delay: 2, max_attempts: 9){|sub| sub.state == "available"}
 #subnet0.wait_until(delay: 2, max_attempts: 9){|sub| until sub.state == "available"; puts "waiting for subnet 0"; puts subnet0.state.inspect ; sleep 1; end}
 puts "create subnet 20"
 subnet20.wait_until(delay: 2, max_attempts: 3){|sub| sub.state == "available"}
-#subnet0.wait_until({"delay" => 2, "max_attempts" => 3}){|sub| until sub.subnet_id == "available"; sleep 1}
-#subnet20.wait_until({"delay" => 2, "max_attempts" => 3}){|sub| until sub.subnet_id == "available"; sleep 1}
 puts "subnets are available"
 
 
@@ -68,38 +66,110 @@ if @options[:verbose]
 end
 
 @ec2clnt = Aws::EC2::Client.new(region: 'us-west-1')
-ni_resp = @ec2clnt.create_network_interface({
-  description: "netface", 
+ni_resp0 = @ec2clnt.create_network_interface({
+  description: "netface0", 
   subnet_id: subnet0.subnet_id
 })
+ni_resp20 = @ec2clnt.create_network_interface({
+  description: "netface1", 
+  subnet_id: subnet20.subnet_id
+})
 
-##### create instance
-ec2res = Aws::EC2::Resource.new(region: 'us-west-1')
+##### create OpsWorks stack
+if @options[:verbose]
+	puts "create OpsWorks stack"
+end
+clnt = Aws::OpsWorks::Client.new( region: 'us-west-1')
 instanceprofilearn = 'arn:aws:iam::718573612756:instance-profile/aws-opsworks-ec2-role '
 servicerolearn = 'arn:aws:iam::718573612756:role/aws-opsworks-service-role'
 itype = "t2.micro"
 imgid = "ami-b73d6cd7"
+stackconfigmgr = Aws::OpsWorks::Types::StackConfigurationManager.new(name: 'Chef', version: '12') # override dflt chef config (v. 11.4)
+rslt = clnt.create_stack(name: stackname, region: 'us-west-1', default_instance_profile_arn: 
+			instanceprofilearn, service_role_arn: servicerolearn, configuration_manager: stackconfigmgr)
+stackid = rslt.stack_id
+if @options[:verbose]
+	puts "Created stack #{stackname}, id = #{stackid}"
+end
+rslt = clnt.create_layer(
+	stack_id: stackid, 
+	type: "custom",
+	name: "klayer",
+	shortname: "kk"
+)
+klayerid = rslt.layer_id
+rslt = clnt.create_layer(
+	stack_id: stackid, 
+	type: "custom",
+	name: "jlayer",
+	shortname: "jj"
+)
+jlayerid = rslt.layer_id
+
+##### create instances
+=begin
+ec2res = Aws::EC2::Resource.new(region: 'us-west-1')
 
 ec2res.create_instances({
 	image_id: imgid,
 	min_count: 1,
   max_count: 1,
 	instance_type: itype,
-	network_interfaces: [{device_index: 0, network_interface_id:  ni_resp.network_interface.network_interface_id}]
+	network_interfaces: [{device_index: 0, network_interface_id:  ni_resp0.network_interface.network_interface_id}]
 })
-puts "All build; hit <Enter> to destroy"
+ec2res.create_instances({
+	image_id: imgid,
+	min_count: 1,
+  max_count: 1,
+	instance_type: itype,
+	network_interfaces: [{device_index: 0, network_interface_id:  ni_resp20.network_interface.network_interface_id}]
+})
+
+clnt.create_instance ({
+	stack_id: stackid, 
+  layer_ids: [jlayerid],
+  instance_type: itype,
+	hostname: 'krameserver',
+	subnet_id: 'subnet-fe56699b',
+	ami_id: 'ami-be0c59de',
+	availability_zone: 'us-west-1a',
+	#:virtualization_type: 'paravirtual',
+	os: 'Ubuntu 14.04 LTS'
+})
+=end
+resp = clnt.create_instance({
+  stack_id: stackid,
+  layer_ids: [klayerid],
+  instance_type: itype,
+  hostname: "krameserver",
+	os: 'Ubuntu 14.04 LTS',
+	ami_id: 'ami-be0c59de',
+  ssh_key_name: "mrkey",
+	availability_zone: 'us-west-1a',
+#  virtualization_type: "String",
+  subnet_id: subnet0.subnet_id
+})
+
+puts "All built; hit <Enter> to destroy"
 STDIN.gets
 
+tokill = []
 puts "terminate any instances"
 vpc.subnets.each do |sn|
 	sn.instances.each do |inst|
+		tokill.push inst
 		inst.terminate
-		inst.wait_until_terminated
 	end
+end
+tokill.each do |inst|
+	inst.wait_until_terminated
 end
 
 @ec2clnt.delete_network_interface({
-	network_interface_id: ni_resp.network_interface.network_interface_id 
+	network_interface_id: ni_resp0.network_interface.network_interface_id 
+})
+@ec2clnt.delete_network_interface({
+	network_interface_id: ni_resp20.network_interface.network_interface_id 
 })
 def loseTheSubnet(subnetInst)
 	if @options[:verbose]
@@ -125,118 +195,3 @@ ensure
 	sleep 2
 	vpc.delete
 end
-exit
-##### create OpsWorks stack
-if @options[:verbose]
-	puts "create OpsWorks stack"
-end
-clnt = Aws::OpsWorks::Client.new( region: 'us-west-1')
-stackconfigmgr = Aws::OpsWorks::Types::StackConfigurationManager.new(name: 'Chef', version: '12') # override dflt chef config (v. 11.4)
-rslt = clnt.create_stack(name: stackname, region: 'us-west-1', default_instance_profile_arn: 
-			instanceprofilearn, service_role_arn: servicerolearn, configuration_manager: stackconfigmgr)
-stackid = rslt.stack_id
-if @options[:verbose]
-	puts "Created stack #{stackname}, id = #{stackid}"
-end
-rslt = clnt.create_layer(
-	stack_id: stackid, 
-	type: "custom",
-	name: "klayer",
-	shortname: "kk"
-)
-if @options[:verbose]
-	puts "Created layer \"klayer\", result: #{rslt.inspect}"
-end
-rslt = clnt.create_layer(
-	stack_id: stackid, 
-	type: "custom",
-	name: "jlayer",
-	shortname: "jj"
-)
-jlayerid = rslt.layer_id
-if @options[:verbose]
-	
-end
-if @options[:verbose]
-	puts "Created layer \"jlayer\", result: #{rslt.inspect}"
-end
-
-#resp = ec2 = Aws::EC2::Client.new(region: 'us-west-1')
-##### create instance
-ec2res = Aws::EC2::Resource.new(region: 'us-west-1')
-instanceprofilearn = 'arn:aws:iam::718573612756:instance-profile/aws-opsworks-ec2-role '
-servicerolearn = 'arn:aws:iam::718573612756:role/aws-opsworks-service-role'
-itype = "t2.micro"
-imgid = "ami-b73d6cd7"
-
-ec2res.create_instances({
-	image_id: imgid,
-	min_count: 1,
-  max_count: 1,
-	instance_type: itype,
-	network_interfaces: [{subnet_id: subnet0.subnet_id}]
-})
-=begin
-clnt.create_instance ({
-	stack_id: stackid, 
-  layer_ids: [jlayerid],
-  instance_type: itype,
-	hostname: 'krameserver',
-	subnet_id: 'subnet-fe56699b',
-	ami_id: 'ami-be0c59de',
-	availability_zone: 'us-west-1a',
-	#:virtualization_type: 'paravirtual',
-	os: 'Ubuntu 14.04 LTS'
-})
-=end
-exit
-instanceprofilearn = 'arn:aws:iam::718573612756:instance-profile/aws-opsworks-ec2-role '
-servicerolearn = 'arn:aws:iam::718573612756:role/aws-opsworks-service-role'
-clnt = Aws::OpsWorks::Client.new( region: 'us-west-1')
-#clnt = Aws::OpsWorks::Client.new( region: 'us-west-1', http_wire_trace: true )
-stackconfigmgr = Aws::OpsWorks::Types::StackConfigurationManager.new(name: 'Chef', version: '12') # override dflt chef config (v. 11.4)
-rslt = clnt.create_stack(name: stackname, region: 'us-west-1', default_instance_profile_arn: 
-			instanceprofilearn, service_role_arn: servicerolearn, configuration_manager: stackconfigmgr)
-stackid = rslt.stack_id
-puts "Created stack #{stackname}, id = #{stackid}"
-rslt = clnt.create_layer(
-	stack_id: stackid, 
-	type: "custom",
-	name: "klayer",
-	shortname: "kk"
-)
-puts "Created layer \"klayer\", result: #{rslt.inspect}"
-rslt = clnt.create_layer(
-	stack_id: stackid, 
-	type: "custom",
-	name: "jlayer",
-	shortname: "jj"
-)
-jlayerid = rslt.layer_id
-puts "Created layer \"jlayer\", result: #{rslt.inspect}"
-
-#resp = ec2 = Aws::EC2::Client.new(region: 'us-west-1')
-itype = "t2.micro"
-#inst = Aws::OpsWorks::Types::Instance.new({
-#	stack_id: stackid, 
-#  layer_ids: [jlayerid],
-#  instance_type: itype,
-#	hostname: 'krameserver',
-#	subnet_id: 'subnet-fe56699b',
-#	ami_id: 'ami-be0c59de',
-#	os: 'Ubuntu 14.04 LTS'
-#})
-#clnt.create_instance (inst)
-clnt.create_instance ({
-	stack_id: stackid, 
-  layer_ids: [jlayerid],
-  instance_type: itype,
-	hostname: 'krameserver',
-	subnet_id: 'subnet-fe56699b',
-#	ami_id: 'ami-be0c59de',
-	availability_zone: 'us-west-1a',
-	#:virtualization_type: 'paravirtual',
-	os: 'Ubuntu 14.04 LTS'
-})
-puts "instance request: #{resp.inspect}"
-exit
